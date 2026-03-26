2af7bafdcfdcc466a50bdee19490e30844b23708
Wendge3072 <wendge@outlook.com>
2026-03-26 14:00:37 +0800

    demo

--- packet_processor/dev/flexio_pp_dev_tnt3.c
+++ packet_processor/dev/flexio_pp_dev_tnt3.c
@@ -27,16 +27,23 @@ __dpa_global__ void flexio_pp_dev_31(uint64_t thread_arg)
 	// register size_t cyApkt = 0, cyMpkt = __dpa_thread_cycles();
 	while (dtctx != NULL) {
 		while (flexio_dev_cqe_get_owner(this_tenant->rq_cq_ctx.cqe) != this_tenant->rq_cq_ctx.cq_hw_owner_bit) {
-
-			eu_status status = __atomic_load_n(&offload_info[i].status, __ATOMIC_RELAXED);
-			cycle_delta = __dpa_thread_cycles();
-			if (status != EU_OVER) {
+			if (__atomic_load_n(&offload_info[i].status, __ATOMIC_RELAXED) == EU_OVER) {
 				// flexio_dev_thread_reschedule();
-				pp_queue(dtctx, this_thd_ctx, this_tenant);
-				com_step_cq(&(this_tenant->rq_cq_ctx));
-				// continue;
+				continue;
 			}
+			cycle_delta = __dpa_thread_cycles();
+			pp_queue(dtctx, this_thd_ctx, this_tenant);
+			com_step_cq(&(this_tenant->rq_cq_ctx));
 			cycle_delta = __dpa_thread_cycles() - cycle_delta;
+			// eu_status status = __atomic_load_n(&offload_info[i].status, __ATOMIC_RELAXED);
+			// cycle_delta = __dpa_thread_cycles();
+			// if (status != EU_OVER) {
+			// 	// flexio_dev_thread_reschedule();
+			// 	pp_queue(dtctx, this_thd_ctx, this_tenant);
+			// 	com_step_cq(&(this_tenant->rq_cq_ctx));
+			// 	// continue;
+			// }
+			// cycle_delta = __dpa_thread_cycles() - cycle_delta;
 			__atomic_fetch_add(&offload_info[i].busy_cycle, cycle_delta, __ATOMIC_RELAXED);
 			// cyApkt += cycle_delta;
 
@@ -88,23 +95,23 @@ __dpa_global__ void flexio_pp_dev_32(uint64_t thread_arg)
 	// register size_t cyApkt = 0, cyMpkt = __dpa_thread_cycles();
 	while (dtctx != NULL) {
 		while (flexio_dev_cqe_get_owner(this_tenant->rq_cq_ctx.cqe) != this_tenant->rq_cq_ctx.cq_hw_owner_bit) {
-			// if (__atomic_load_n(&offload_info[i].status, __ATOMIC_RELAXED) == EU_OVER) {
-			// 	// flexio_dev_thread_reschedule();
-			// 	continue;
-			// }
-			// cycle_delta = __dpa_thread_cycles();
-			// pp_queue(dtctx, this_thd_ctx, this_tenant);
-			// com_step_cq(&(this_tenant->rq_cq_ctx));
-			// cycle_delta = __dpa_thread_cycles() - cycle_delta;
-			eu_status status = __atomic_load_n(&offload_info[i].status, __ATOMIC_RELAXED);
-			cycle_delta = __dpa_thread_cycles();
-			if (status != EU_OVER) {
+			if (__atomic_load_n(&offload_info[i].status, __ATOMIC_RELAXED) == EU_OVER) {
 				// flexio_dev_thread_reschedule();
-				pp_queue(dtctx, this_thd_ctx, this_tenant);
-				com_step_cq(&(this_tenant->rq_cq_ctx));
-				// continue;
+				continue;
 			}
+			cycle_delta = __dpa_thread_cycles();
+			pp_queue(dtctx, this_thd_ctx, this_tenant);
+			com_step_cq(&(this_tenant->rq_cq_ctx));
 			cycle_delta = __dpa_thread_cycles() - cycle_delta;
+			// eu_status status = __atomic_load_n(&offload_info[i].status, __ATOMIC_RELAXED);
+			// cycle_delta = __dpa_thread_cycles();
+			// if (status != EU_OVER) {
+			// 	// flexio_dev_thread_reschedule();
+			// 	pp_queue(dtctx, this_thd_ctx, this_tenant);
+			// 	com_step_cq(&(this_tenant->rq_cq_ctx));
+			// 	// continue;
+			// }
+			// cycle_delta = __dpa_thread_cycles() - cycle_delta;
 			__atomic_fetch_add(&offload_info[i].busy_cycle, cycle_delta, __ATOMIC_RELAXED);
 			// cyApkt += cycle_delta;
 
