7a28f9b9560f28c755ff7e54aaaa083761e7a443
Wendge3072 <wendge@outlook.com>
2026-03-30 20:26:54 +0800

    demo

--- packet_processor/dev/flexio_pp_dev_utils.c
+++ packet_processor/dev/flexio_pp_dev_utils.c
@@ -40,7 +40,8 @@ int pp_queue(struct flexio_dev_thread_ctx *dtctx, struct dpa_thread_context* thi
 	if (__atomic_load_n(&offload_info[thd_id].restrict_tenant[tenant_id], __ATOMIC_RELAXED) == 1) {
 		__dpa_thread_memory_writeback();
 		flexio_dev_dbr_rq_inc_pi(tenant->rq_ctx.rq_dbr);
-		return -1;
+		// return -1;
+		return tenant_id;
 	}
 
 	swap_mac(rq_data);
