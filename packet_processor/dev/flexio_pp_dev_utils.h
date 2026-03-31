50b8c6a5afa40d8026b34e05df0b9407d5576bca
Wendge3072 <wendge@outlook.com>
2026-03-31 15:40:23 +0800

    demo

--- packet_processor/dev/flexio_pp_dev_utils.h
+++ packet_processor/dev/flexio_pp_dev_utils.h
@@ -96,7 +96,7 @@ static size_t scheduler_num;
 static uint32_t thrput_quantum = 15625;
 static uint16_t q_packet = 670;
 static uint32_t thrput_weights[MAX_TENANT_NUM] = {30, 30};
-static uint32_t cycle_weights[MAX_TENANT_NUM] = {10, 5};
+static uint32_t cycle_weights[MAX_TENANT_NUM] = {10, 10};
 
 flexio_dev_status_t change_status(uint16_t thd_id, eu_status old_status, eu_status new_status);
 
