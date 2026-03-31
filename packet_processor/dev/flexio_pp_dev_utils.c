cf4305d5872cef8c08ebe9bf4c12608f5db21248
Wendge3072 <wendge@outlook.com>
2026-03-31 15:08:00 +0800

    demo

--- packet_processor/dev/flexio_pp_dev_utils.c
+++ packet_processor/dev/flexio_pp_dev_utils.c
@@ -37,13 +37,14 @@ int pp_queue(struct flexio_dev_thread_ctx *dtctx, struct dpa_thread_context* thi
 	uint32_t tenant_indicator = *(uint32_t *)(rq_data + 46);
 	int tenant_id = (tenant_indicator == 0) ? 0 : 1;
 
-	// if (__atomic_load_n(&offload_info[thd_id].restrict_tenant[tenant_id], __ATOMIC_RELAXED) == 1) {
-	// 	__dpa_thread_memory_writeback();
-	// 	flexio_dev_dbr_rq_inc_pi(tenant->rq_ctx.rq_dbr);
-	// 	// *result = 0;
-	// 	// return tenant_id;
-	// 	return -1;
-	// }
+	if (__atomic_load_n(&offload_info[thd_id].restrict_tenant[tenant_id], __ATOMIC_RELAXED) == 1) {
+		__dpa_thread_memory_writeback();
+		flexio_dev_dbr_rq_inc_pi(tenant->rq_ctx.rq_dbr);
+		com_step_cq(&(tenant->rq_cq_ctx));
+		// *result = 0;
+		// return tenant_id;
+		return -1;
+	}
 
 	swap_mac(rq_data);
 	// volatile uint_test checksum = calculate_checksum_nrnd(rq_data, data_sz / 4, 1);
