b5805c4c7e6907c6f50a91a5d03142262e597e14
Wendge3072 <wendge@outlook.com>
2026-04-01 20:08:05 +0800

    version1

--- packet_processor/dev/flexio_pp_dev_utils.c
+++ packet_processor/dev/flexio_pp_dev_utils.c
@@ -38,27 +38,28 @@ int pp_queue(struct flexio_dev_thread_ctx *dtctx, struct dpa_thread_context* thi
 	int tenant_id = (tenant_indicator == 0) ? 0 : 1;
 	struct dpa_sche_context* sch_ctx = offload_info[thd_id].sch_ctx;
 
-	if (__atomic_load_n(&sch_ctx->restrict_tenant[tenant_id], __ATOMIC_RELAXED) == 1) {
-		// __dpa_thread_memory_writeback();
-		flexio_dev_dbr_rq_inc_pi(tenant->rq_ctx.rq_dbr);
-		com_step_cq(&(tenant->rq_cq_ctx));
-		// __atomic_fetch_add(&offload_info[thd_id].restrict_pkts[tenant_id], 1, __ATOMIC_RELAXED);
-		// *result = 0;
-		// return tenant_id;
-		return -1;
-	}
+	// if (__atomic_load_n(&sch_ctx->restrict_tenant[tenant_id], __ATOMIC_RELAXED) == 1) {
+	// 	// __dpa_thread_memory_writeback();
+	// 	flexio_dev_dbr_rq_inc_pi(tenant->rq_ctx.rq_dbr);
+	// 	com_step_cq(&(tenant->rq_cq_ctx));
+	// 	// __atomic_fetch_add(&offload_info[thd_id].restrict_pkts[tenant_id], 1, __ATOMIC_RELAXED);
+	// 	// *result = 0;
+	// 	// return tenant_id;
+	// 	return -1;
+	// }
 
-	swap_mac(rq_data);
+	// swap_mac(rq_data);
+
 	// volatile uint_test checksum = calculate_checksum_nrnd(rq_data, data_sz / 4, 1);
 	// *result = calculate_checksum_nrnd(rq_data + 42, (data_sz - 42) / 4, 1);
 
-	swqe = &(tenant->sq_ctx.sq_ring[(tenant->sq_ctx.sq_wqe_seg_idx + 2) & SQ_IDX_MASK]);
-	tenant->sq_ctx.sq_wqe_seg_idx += 4;
-	flexio_dev_swqe_seg_mem_ptr_data_set(swqe, data_sz, tenant->rq_lkey, (uint64_t)rq_data);
+	// swqe = &(tenant->sq_ctx.sq_ring[(tenant->sq_ctx.sq_wqe_seg_idx + 2) & SQ_IDX_MASK]);
+	// tenant->sq_ctx.sq_wqe_seg_idx += 4;
+	// flexio_dev_swqe_seg_mem_ptr_data_set(swqe, data_sz, tenant->rq_lkey, (uint64_t)rq_data);
 	
 	/* Ring DB */
 	__dpa_thread_memory_writeback();
-	flexio_dev_qp_sq_ring_db(dtctx, ++tenant->sq_ctx.sq_pi, tenant->sq_ctx.sq_number);
+	// flexio_dev_qp_sq_ring_db(dtctx, ++tenant->sq_ctx.sq_pi, tenant->sq_ctx.sq_number);
 	flexio_dev_dbr_rq_inc_pi(tenant->rq_ctx.rq_dbr);
 	com_step_cq(&(tenant->rq_cq_ctx));
 
