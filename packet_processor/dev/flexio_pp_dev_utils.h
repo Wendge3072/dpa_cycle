fabc8154efb575d4178d740d6890319a0d9fd4a2
Wendge3072 <wendge@outlook.com>
2026-04-02 14:29:55 +0800

    version1

--- packet_processor/dev/flexio_pp_dev_utils.h
+++ packet_processor/dev/flexio_pp_dev_utils.h
@@ -92,7 +92,7 @@ extern struct offload_dispatch_info offload_info[190];
 #define report_cycle_usage 1
 #define report_pkt_usage 0
 
-#define CHECK_BUDGET_AT_WORKER 1 // 1 for Worker-side cycle monitoring, 0 for Scheduler-side inline monitoring
+#define CHECK_BUDGET_AT_WORKER 0 // 1 for Worker-side cycle monitoring, 0 for Scheduler-side inline monitoring
 
 #define SCHED_PERIOD_CYCLES DPA_FREQ_HZ / 1000
 #define SCHED_PERIOD_CYCLES_PERCENT SCHED_PERIOD_CYCLES / 100
