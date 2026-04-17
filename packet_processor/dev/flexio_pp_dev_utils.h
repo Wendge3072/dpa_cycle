0a1857a95c624f1d00632d09588281bd1732dc4c
Wendge3072 <wendge@outlook.com>
2026-04-17 14:47:43 +0800

    final

--- packet_processor/dev/flexio_pp_dev_utils.h
+++ packet_processor/dev/flexio_pp_dev_utils.h
@@ -103,7 +103,7 @@ extern struct offload_dispatch_info offload_info[190];
 
 static uint32_t cycle_weights[MAX_TENANT_NUM] = {100, 100};
 // static uint32_t bandwidth_weights[MAX_TENANT_NUM] = {72, 18};
-static uint32_t bandwidth_weights[MAX_TENANT_NUM] = {45, 45};
+static uint32_t bandwidth_weights[MAX_TENANT_NUM] = {70, 20};
 
 void spin_on_status(uint16_t thd_id, eu_status expected_status);
 
