390e95d7edc0ee7eb52e55221e4db5311a096e72
Wendge3072 <wendge@outlook.com>
2026-04-27 15:42:48 +0800

    wc

--- packet_processor/dev/flexio_pp_dev_utils.h
+++ packet_processor/dev/flexio_pp_dev_utils.h
@@ -43,7 +43,7 @@ sch 线程 rollover 模式切换开关 - 是否切换到 work-conserving 模式
 */
 #define SCH_ROLLOVER_WORK_CONSERVING 1
 
-#define DEFAULT_LINK_BANDWIDTH_BPS 90000000000ULL
+#define DEFAULT_LINK_BANDWIDTH_BPS 80000000000ULL
 #define MAX_CYCLE_PERCENTAGE 7800
 #define MAX_CYCLE_TOTAL 10000
 #define WORKER_BATCH_SIZE 1048576UL
@@ -60,9 +60,9 @@ sch 线程 rollover 模式切换开关 - 是否切换到 work-conserving 模式
 #define TENANT_RESTRICT_CYCLE 1
 #define TENANT_RESTRICT_BW 2
 
-static uint32_t cycle_weights[MAX_TENANT_NUM] = {50, 50};
+static uint32_t cycle_weights[MAX_TENANT_NUM] = {60, 40};
 // static uint32_t bandwidth_weights[MAX_TENANT_NUM] = {80, 10}; 
-static uint32_t bandwidth_weights[MAX_TENANT_NUM] = {45, 45};
+static uint32_t bandwidth_weights[MAX_TENANT_NUM] = {40, 60};
 
 struct flexio_dpa_dev_queue {
 	/* lkey - local memory key */
